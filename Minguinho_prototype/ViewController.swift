88a6eb2b9f04ca24875b1baf40b8353069083e62
boychaboy <hoon2j@gmail.com>
2019-06-06 13:27:23 +0900

    Initial Commit

---
+++ Minguinho_prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minguinho_prototype
+//
+//  Created by Eric Jeong on 06/06/2019.
+//  Copyright © 2019 boychaboy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
